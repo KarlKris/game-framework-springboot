a585e1d652818642a92e1ee33c2ff56754151951
KrisKarl <953591544@qq.com>
2022-02-25 16:07:39 +0800

    业务线程池初构

---
+++ game-common/src/main/java/com/li/gamecommon/thread/GenericSerializedExecutorService.java
@@ -0,0 +1,43 @@
+﻿package com.li.gamecommon.thread;
+
+import java.util.Queue;
+
+/**
+ * 业务线程池实现类
+ * @author li-yuanwen
+ * @date 2022/2/8
+ */
+public class GenericSerializedExecutorService  {
+
+
+
+
+
+    /** 任务源对象 **/
+    private final class IdTaskContext {
+
+        /** 状态 **/
+        volatile boolean execute;
+        /** 待执行任务队列 **/
+        Queue<Runnable> waitToExecTaskQueue;
+        /** 新任务队列 **/
+        Queue<Runnable> newTaskQueue;
+
+    }
+
+
+    /** 工人 **/
+    private final class TaskWorker implements Runnable {
+
+        /** 工作线程 **/
+        Thread thread;
+
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
+}
---
+++ game-common/src/main/java/com/li/gamecommon/thread/SerializedExecutorService.java
@@ -0,0 +1,28 @@
+﻿package com.li.gamecommon.thread;
+
+import java.util.concurrent.Future;
+
+/**
+ * 业务线程池接口 支持串行化执行同一请求源的线程池
+ * @author li-yuanwen
+ * @date 2022/2/8
+ */
+public interface SerializedExecutorService {
+
+
+    /**
+     * 提交任务
+     * @param id 请求源标识
+     * @param task 任务
+     * @return /
+     */
+    Future<?> submit(Long id, Runnable task);
+
+    /**
+     * 提交任务
+     * @param task
+     * @return /
+     */
+    Future<?> submit(Runnable task);
+
+}
